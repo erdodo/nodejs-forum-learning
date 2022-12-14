606510aff41ef5ad5cf7c06cea16afa47f2db439
erdodo <erdoganyesil3@gmail.com>
2022-12-14 23:25:08 +0300

    vercel entegration

--- src/swagger/autogen.js
+++ src/swagger/autogen.js
@@ -1,4 +1,4 @@
-const swaggerAutogen = require("swagger-autogen")();
+const swaggerAutogen = require("swagger-autogen")({ language: "tr", openapi: "3.0.3" });
 
 const outputFile = "./output.json";
 const endpointsFiles = ["../routes/index.js"];
@@ -7,10 +7,18 @@ const doc = {
   info: {
     title: "Node Simple",
     description: "Açıklama",
-    version: "0.0.1",
+    version: "0.0.2",
   },
-  host: "localhost:3000/api/v1",
-  schemes: ["http"],
+
+  servers: [
+    {
+      url: "localhost:3000/api/v1",
+    },
+    {
+      url: "https://combative-cyan-pocket.cyclic.app/api/v1",
+    },
+  ],
+  schemes: ["http", "https"],
   tags: [
     {
       name: "test",
